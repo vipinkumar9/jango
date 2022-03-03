f3cae13ccbde4241116febc6adc81f9aaf9a2594
Vipin Kumar <vipin.kumar.webmail@gmail.com>
2022-03-03 12:35:51 +0530

    Added Pagination

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from api.models import *
@@ -12,7 +12,7 @@ def index(request):
 
 
 def create_project(request):
-    NAME = request.POST.get('projectName')
+    NAME = request.POST.get('name')
     id_rand  = ''.join(random.choices(
                     string.ascii_letters + string.digits, k=10))
     try:
@@ -24,8 +24,8 @@ def create_project(request):
 
 def create_issue(request):
     try:
-        obj = Project.objects.get(project_id=request.POST.get('projectId'))
-        Issue(issue_id = obj, title=request.POST.get('issueTitle'),label=request.POST.get('issueLabel'),assignee=request.POST.get('issueAssignee'),type=request.POST.get('issueType'),sprint=request.POST.get('issueSprint'),status=request.POST.get('issueStatus')).save()
+        obj = Project.objects.get(project_id=request.POST.get('project_id'))
+        Issue(issue_id = obj, title=request.POST.get('title'),label=request.POST.get('labels'),assignee=request.POST.get('assignee'),type=request.POST.get('type'),sprint=request.POST.get('sprint'),status=request.POST.get('status')).save()
         return HttpResponse(json.dumps({"status": "200","Issue":"created"}), content_type='application/json')
 
     except Exception as e:
@@ -40,7 +40,7 @@ def get_all_projects(request):
     return HttpResponse(json.dumps({"Projects": d}), content_type='application/json')
 
 def get_project_details(request):
-    project_id = request.POST.get('projectId')
+    project_id = request.POST.get('project_id')
     obj = Project.objects.get(project_id=project_id)
     issues = []
     for i in Issue.objects.filter(issue_id=obj).all():
@@ -49,10 +49,12 @@ def get_project_details(request):
     return HttpResponse(json.dumps({"status":"200","projectId":project_id,"projectName":obj.name,"Issues":issues}), content_type='application/json')
 
 def get_all_issue_of_project(request):
-    project_id = request.POST.get('projectId')
+    project_id = request.POST.get('project_id')
+    limit = request.POST.get('limit')
+    offset = request.POST.get('offset')
     obj = Project.objects.get(project_id=project_id)
     issues = []
-    for i in Issue.objects.filter(issue_id=obj).all():
+    for i in Paginator(Issue.objects.filter(issue_id=obj).all(), limit).page(offset):
         issues.append({"Title":i.title,"Label":i.label,"Type":i.type,"Sprint":i.sprint,"Status":i.status})
     
     return HttpResponse(json.dumps({"status":"200","Issues":issues}), content_type='application/json')
